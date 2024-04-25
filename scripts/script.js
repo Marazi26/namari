0c2c4c6ef8f09b958f94670046bf10181717cbf9
M0.26 <141484399+Marazi26@users.noreply.github.com>
2024-04-25 20:20:01 +0400

    20:19

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,36 @@ function fixedNav(){
     let socials = document.querySelector(".nav-socials")
     let scrollValue = window.scrollY;
 
-    if(scrollValue > 600){
-        nav.style.transition = ".5s linear"
+    if(scrollValue > 700){
+        
         nav.style.position = "fixed"
         nav.style.borderBottom = "1px solid #cccccc"
         nav.style.top = "0"
-        nav.style.left = "1.4rem"
-        nav.style.right = "1.4rem"
+        nav.style.left = "0"
+        nav.style.right = "0"
+
         nav.style.backgroundColor = "#ffffff"
         nav.style.display = 'flex';
         nav.style.justifyContent = 'center';
         nav.style.alignItems = 'center';
-        nav.style.zIndex = '0'
+        nav.style.zIndex = "7"
         nav.style.paddingBottom = '1rem'
         socials.style.color = "#cccccc"
-    } else {
+
+        nav.style.transitionDuration = "2s"
+        nav.style.transitionTimingFunction = "ease-out-in"
+        nav.style.opacity = "1"
+        nav.style.visibility = "visible"
+        nav.style.transform = "translateY(0)"
+    } else if (scrollValue > 600) {
+        nav.style.transform = "translateY(-100%)"
+    }
+    else{
+        nav.style.position = "static"
+        nav.style.border = "none"
         nav.style.border = "none"
         nav.style.position = "static"
+        nav.style.transform = "translateY(0)"
     }
 }
 window.addEventListener("scroll", fixedNav)
@@ -34,6 +47,10 @@ function menuLiColor(){
         menuItems.forEach(item => {
           item.style.color = '#cccccc';
         });
+    } else {
+        menuItems.forEach(item => {
+            item.style.color = '#111111';
+          });
     }
 }
 window.addEventListener("scroll", menuLiColor)
